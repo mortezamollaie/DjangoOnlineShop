740b4ac57a04ac39e1c9b54197330d15268f9d25
morteza <mm.gov.1381@gmail.com>
2024-03-14 15:49:46 +0330

    cart remove

--- orders/urls.py
+++ orders/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'orders'
 urlpatterns = [
     path('cart/', views.CartView.as_view(), name='cart'),
-    path('cart/add/<int:product_id>/', views.CartAddView.as_view(), name='cart_add')
+    path('cart/add/<int:product_id>/', views.CartAddView.as_view(), name='cart_add'),
+    path('cart/remove/<int:product_id>/', views.CartRemoveView.as_view(), name='cart_remove'),
 ]
