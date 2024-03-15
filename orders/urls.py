ecd318fcfdae89e49348313c7ffffb1dd9990fbc
morteza <mm.gov.1381@gmail.com>
2024-03-16 00:37:19 +0330

    adding coupon

--- orders/urls.py
+++ orders/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('cart/add/<int:product_id>/', views.CartAddView.as_view(), name='cart_add'),
     path('cart/remove/<int:product_id>/', views.CartRemoveView.as_view(), name='cart_remove'),
     path('pay/<int:order_id>/', views.OrderPayView.as_view(), name='order_pay'),
-    path('verify/', views.OrderVerifyView.as_view(), name='order_verify')
+    path('verify/', views.OrderVerifyView.as_view(), name='order_verify'),
+    path('apply/<int:order_id>/', views.CouponApplyView.as_view(), name='apply_coupon'),
 ]
