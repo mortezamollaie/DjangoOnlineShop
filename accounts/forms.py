3c6b8bc47922a5181536562a628cfe71fcba0b65
mortezamollaie <mm.gov.1381@gmail.com>
2025-08-04 23:11:47 +0330

    fix: fixing error on creating new user

--- accounts/forms.py
+++ accounts/forms.py
@@ -16,10 +16,10 @@ class UserCreationForm(forms.ModelForm):
         cd = self.cleaned_data
         if cd['password1'] and cd['password2'] and cd['password1'] != cd['password2']:    
             return ValidationError('password dont match')
-        return password2 
+        return cd['password2'] 
 
     def save(self, commit=True):
-        user = super.save(commit=False)
+        user = super().save(commit=False)
         user.set_password(self.cleaned_data['password1'])
         if commit:
             user.save()
