588bd8c20ac1d3218de8aee84eee5ecf508651cb
mortezamollaie <mm.gov.1381@gmail.com>
2025-08-06 13:15:49 +0330

    feat: showing product on home page

--- home/views.py
+++ home/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
+from .models import Product
 
 class HomeView(View):
     template_name = 'home/home.html'
 
     def get(self, request):
-        return render(request, self.template_name)
+        products = Product.objects.filter(available=True)
+        return render(request, self.template_name, {'products': products})
+    
+
+class ProductDetailView(View):
+    def get(self, request, slug):
+        product = get_object_or_404(Product, slug=slug)
+        return render(request, 'home/detail.html', {'product': product})
