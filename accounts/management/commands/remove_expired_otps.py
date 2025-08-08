acb17721379db49312b42d384c4ec8fc20201389
mortezamollaie <mm.gov.1381@gmail.com>
2025-08-08 22:38:54 +0330

    fix: fixing warning on remove expired otps

--- accounts/management/commands/remove_expired_otps.py
+++ accounts/management/commands/remove_expired_otps.py
@@ -1,13 +1,14 @@
 from django.core.management.base import BaseCommand
 from accounts.models import OtpCode
 from datetime import datetime, timedelta
+import pytz
 
 
 class Command(BaseCommand):
     help = 'remove all expired otp codes'
 
     def handle(self, *args, **options):
-        expiration_time = datetime.now() - timedelta(minutes=2)
+        expiration_time = datetime.now(tz=pytz.timezone('Asia/Tehran')) - timedelta(minutes=2)
         expired_otps = OtpCode.objects.filter(created__lt=expiration_time)
         expired_otps.delete()
         self.stdout.write(self.style.SUCCESS('Successfully removed expired OTP codes.'))
