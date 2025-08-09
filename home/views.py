60e27c510fa0c5669685d6cd490f45f644ca4094
mortezamollaie <mm.gov.1381@gmail.com>
2025-08-09 11:23:11 +0330

    feat: implement filtering by category

--- home/views.py
+++ home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
-from .models import Product
+from .models import Product, Category
 from . import tasks 
 from django.contrib import messages
 from utils import IsAdminUserMixin
@@ -9,10 +9,14 @@ from utils import IsAdminUserMixin
 class HomeView(View):
     template_name = 'home/home.html'
 
-    def get(self, request):
+    def get(self, request, category_slug=None):
         products = Product.objects.filter(available=True)
-        return render(request, self.template_name, {'products': products})
-    
+        categories = Category.objects.all()
+        if category_slug:
+            category = get_object_or_404(Category, slug=category_slug)
+            products = products.filter(category=category)
+        return render(request, self.template_name, {'products': products, 'categories': categories})
+
 
 class ProductDetailView(View):
     def get(self, request, slug):
